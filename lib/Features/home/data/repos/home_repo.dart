8447ef9d74b83e552a2e77b7cdc0ea66634a6613
Mahmou-d6356567377 <131625133+Mahmou-d6356567377@users.noreply.github.com>
2024-06-22 20:57:08 +0300

    create api service

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,7 +1,11 @@
 
 
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/errors/Failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class Homerepo{
 
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+    Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
+import 'package:bookly/core/errors/Failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements Homerepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+  
+}
